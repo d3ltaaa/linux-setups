50081faa834ebc737a57305415b43aa2dd4b7439
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-15 13:11:46 +0100

    Create install-synergy.sh

---
+++ install-synergy.sh
@@ -0,0 +1,4 @@
+# installing synergy
+cd Downloads \
+curl -O https://api2.prod.symless.com/aws-downloads/synergy/v1-core-standard/1.14.6-snapshot.88fdd263/synergy_1.14.6-snapshot.88fdd263.flatpak \
+flatpak install synergy_1.14.6-snapshot.88fdd263.flatpak \
