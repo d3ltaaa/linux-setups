0fca0f9369eefa4793d058d2e1493d75454844ad
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-15 12:34:43 +0100

    Update install-remnote.sh

--- fedora-kde-install/install-remnote.sh
+++ fedora-kde-install/install-remnote.sh
@@ -1,5 +1,20 @@
 cd /home/falk
 mkdir .AppImages
-cd .AppImages
+
+cd /home/falk/.AppImages
 wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
 chmod +x remnote.AppImage
+
+cd /home/falk/.local/share/icons
+wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/remnote-install/image.png -O image.png
+
+cd /home/falk/.local/share/applications
+
+cat > ~/.local/share/applications/remnote.desktop <<EOL
+[Desktop Entry]
+Type=Application
+Name=RemNote
+Exec=/home/falk/.AppImages/remnote.AppImage %U
+Icon=/opt/remnote/remnote.png
+Terminal=false
+EOL
