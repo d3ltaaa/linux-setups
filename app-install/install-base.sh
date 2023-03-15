57ca94e6c3f32f6a6d332ae1d7732101e188f905
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-15 17:53:41 +0100

    Update install-base.sh

--- app-install/install-base.sh
+++ app-install/install-base.sh
@@ -1,4 +1,4 @@
-echo "version 4\n"
+echo "version 5\n"
 cd /home/falk/Downloads 
 dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
@@ -19,31 +19,39 @@ dnf install \
   vlc \
   xournalpp 
   
+echo "\n make repos\n"
+  
 mkdir -p /home/falk/.local/share/icons 
 mkdir -p /home/falk/.local/share/applications 
 mkdir -p /home/falk/.local/share/AppImages 
 mkdir -p /home/falk/Downloads
 
-
+echo "\n spotify \n"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.spotify.Client
   
 cd /home/falk/Downloads
 
+echo "\n synergy \n"
 curl -O https://api2.prod.symless.com/aws-downloads/synergy/v1-core-standard/1.14.6-snapshot.88fdd263/synergy_1.14.6-snapshot.88fdd263.flatpak
 flatpak install synergy_1.14.6-snapshot.88fdd263.flatpak
+
+echo "\n remnote \n"
 cd /home/falk/.local/share/AppImages
 wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
 chmod +x remnote.AppImage
 
+echo "\n remnote-icon \n"
 cd /home/falk/.local/share/icons
 wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote-install/remnote.png
+
+echo "\n remnote-desktop \n"
 
 echo "[Desktop Entry]
 Type=Application
 Name=Remnote
 Exec=/home/falk/.local/share/AppImages/remnote.AppImage
 Icon=/home/falk/.local/share/icons/remnote.png
-Terminal=false " | tee ~/.local/share/applications/remnote.desktop > /dev/null
+Terminal=false " | tee /home/falk/.local/share/applications/remnote.desktop > /dev/null
 
 chmod +x remnote.desktop
