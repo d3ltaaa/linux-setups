158837ce7364c1fe8d4fcfb7246ba10669da1ed6
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-15 17:04:55 +0100

    Update install-base.sh

--- app-install/install-base.sh
+++ app-install/install-base.sh
@@ -13,7 +13,8 @@ dnf install \
   timeshift \
   firefox \
   flatpak \
-  wget \
+  wget 
+  
 mkdir /home/falk/.local/share/icons 
 mkdir /home/falk/.local/share/applications 
 mkdir /home/falk/.local/share/AppImages 
@@ -24,16 +25,21 @@ dnf install \
   xournalpp \
   spotify-client \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
-  discord \
+  discord 
+  
 cd /home/falk/Downloads
+
 curl -O https://api2.prod.symless.com/aws-downloads/synergy/v1-core-standard/1.14.6-snapshot.88fdd263/synergy_1.14.6-snapshot.88fdd263.flatpak
 flatpak install synergy_1.14.6-snapshot.88fdd263.flatpak
 cd /home/falk/.local/share/AppImages
 wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
 chmod +x remnote.AppImage
+
 cd /home/falk/.local/share/icons
 wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/remnote-install/remnote.png
+
 cd /home/falk/.local/share/applications 
+
 cat > ~/.local/share/applications/remnote.desktop <<EOL 
 [Desktop Entry]
 Type=Application
@@ -42,4 +48,5 @@ Exec=/home/falk/.local/share/AppImages/remnote.AppImage
 Icon=/home/falk/.local/share/icons/remnote.png
 Terminal=false
 EOL 
+
 chmod +x remnote.desktop
