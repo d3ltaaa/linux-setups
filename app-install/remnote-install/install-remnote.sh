3c84f34bd06d8143bf5ef8b436852cf7850992bf
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-16 22:56:04 +0100

    working

--- app-install/remnote-install/install-remnote.sh
+++ app-install/remnote-install/install-remnote.sh
@@ -1,19 +1,27 @@
 
+echo "===> downloading remnote.AppImage"
+sleep 3
+mkdir -p /home/falk/.local/share/AppImages
 cd /home/falk/.local/share/AppImages
 wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
 chmod +x remnote.AppImage
 
+echo "===> Downloading remnote icon"
+mkdir -p /home/falk/.local/share/icons
 cd /home/falk/.local/share/icons
-wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/remnote-install/remnote.png
+wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote-install/remnote.png
 
-cd /home/falk/.local/share/applications
-
-cat > ~/.local/share/applications/remnote.desktop <<EOL
+echo "===> Writing to remnote.desktop"
+mkdir -p /home/falk/.local/share/applications
+cat > /home/falk/.local/share/applications/remnote.desktop << EOF
 [Desktop Entry]
 Type=Application
 Name=Remnote
 Exec=/home/falk/.local/share/AppImages/remnote.AppImage
 Icon=/home/falk/.local/share/icons/remnote.png
 Terminal=false
-EOL
+EOF
+
+echo "===> making remnote.desktop executable"
+ls
 chmod +x remnote.desktop
