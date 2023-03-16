9fac81c6517b222914d46c3669731505c5d1da5a
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-16 15:51:24 +0100

    Update fedora-gnome-min-packages.sh

--- fedora-gnome-min-packages.sh
+++ fedora-gnome-min-packages.sh
@@ -1,3 +1,10 @@
+echo "VERSION 0"
+
+echo "===> making required repositories"
+mkdir -p /home/falk/.local/share/AppImages
+mkdir -p /home/falk/.local/share/icons
+mkdir -p /home/falk/.local/share/applications
+
 echo "===> installing @base-x, gnome-shell, gnome-terminal, nautilus, firefox"
 dnf install @base-x gnome-shell gnome-terminal nautilus firefox
 
@@ -26,10 +33,35 @@ dnf install git vim bpytop ncdu ranger neofetch trash-cli speedtest-cli flatpak 
 echo "===> installing timeshift, discord, vlc, xournalpp"
 dnf install timeshift discord vlc xournalpp 
 
+echo "===> downloading spotify"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.spotify.Client
+
 echo "===> install synergy"
 flatpak install https://api2.prod.symless.com/aws-downloads/synergy/v1-core-standard/1.14.6-snapshot.88fdd263/synergy_1.14.6-snapshot.88fdd263.flatpak
 
+echo "===> downloading remnote.AppImage"
+cd /home/falk/.local/share/AppImages
+wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
+chmod +x remnote.AppImage
 
+echo "===> Downloading remnote icon..."
+cd /home/falk/.local/share/icons
+wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote-install/remnote.png
+
+echo "===> Writing to remnote.desktop"
+cat > /home/falk/.local/share/applications/remnote.desktop <<EOL
+[Desktop Entry]
+Type=Application
+Name=Remnote
+Exec=/home/falk/.local/share/AppImages/remnote.AppImage
+Icon=/home/falk/.local/share/icons/remnote.png
+Terminal=false
+EOL
+
+echo "===> making remnote.desktop executable..."
+ls
+chmod +x remnote.desktop
 
 echo "===> make fedora boot into gui"
 systemctl set-default graphical.target
