be91df01e4270e47522672aaead217fc3268100b
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-16 23:07:32 +0100

    Update and rename vlc.sh to non-free.sh

--- app-install/non-free.sh
+++ app-install/non-free.sh
@@ -1,3 +1,3 @@
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf insatll vlc
+dfn install discord
