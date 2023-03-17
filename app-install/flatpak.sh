4f1a4e51faaf5b0064dddb0fedc2b97cc025f677
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-17 20:38:03 +0100

    Update flatpak.sh

--- app-install/flatpak.sh
+++ app-install/flatpak.sh
@@ -1,4 +1,4 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
+flatpak install flathub com.mattjakeman.ExtensionManager
 
-flatpak install flathub org.gnome.Extensions
