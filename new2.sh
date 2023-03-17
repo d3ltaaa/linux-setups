d2ccd20a62bb69aea2340703cf200e55691d25af
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-18 00:14:58 +0100

    Update new2.sh

--- new2.sh
+++ new2.sh
@@ -1,25 +1,146 @@
 #!/bin/bash
 
-# Define the list of programs to install
-programs=(
-    program1
-    program2
-    program3
-)
+
 # Define the list of selected programs
-selected_programs=()
+dnf_selected_programs=()
+free_selected_programs=()
+nonfree_selected_programs=()
+flatpak_selected_programs=()
+appimage_selected_programs=()
+
+
+# Define the list of programs to install
+dnf_programs=(
+  nautilus 
+  gnome-terminal 
+  firefox 
+  gnome-tweaks 
+  @development-tools 
+  vim 
+  git 
+  bpytop 
+  ncdu 
+  ranger 
+  neofetch 
+  trash-cli 
+  speedtest-cli 
+  flatpak 
+  wget 
+  timeshift 
+  xournalpp 
+  ulauncher
+)
+
+# Define the list of programs to install
+free_programs=(
+  vlc
+)
+
+nonfree_programs=(
+  discord
+  
+)
+
+flatpak_programs=(
+    com.mattjakeman.ExtensionManager
+    com.spotify.Client
+)
+
+
+appimage_programs=(
+  remnote
+)
+
+# special dnf programs like synergy are missing
+
 
 # go through each program, ask if it should be installed and add it to the selected programs
-for program in "${programs[@]}"; do
+for program in "${dnf_programs[@]}"; do
       while true; do
-          read -p "Install $program? [y/n] " yn
+          read -p "Install $dnf_program? [y/n] " yn
           case $yn in
-              [Yy]* ) selected_programs+=("$program"); break;;
+              [Yy]* ) dnf_selected_programs+=("$dnf_program"); break;;
               [Nn]* ) break;;
               * ) echo "Enter 'y' or 'n'!";;
           esac
       done
 done
 
-# Install the selected programs
-sudo dnf install -y "${selected_programs[@]}"
+# go through each free program, ask if it should be installed and add it to the selected programs
+for program in "${free_programs[@]}"; do
+      while true; do
+          read -p "Install $free_program? [y/n] " yn
+          case $yn in
+              [Yy]* ) free_selected_programs+=("$free_program"); break;;
+              [Nn]* ) break;;
+              * ) echo "Enter 'y' or 'n'!";;
+          esac
+      done
+done
+
+
+# go through each non-free program, ask if it should be installed and add it to the selected programs
+for program in "${nonfree_programs[@]}"; do
+      while true; do
+          read -p "Install $nonfree_program? [y/n] " yn
+          case $yn in
+              [Yy]* ) nonfree_selected_programs+=("$nonfree_program"); break;;
+              [Nn]* ) break;;
+              * ) echo "Enter 'y' or 'n'!";;
+          esac
+      done
+done
+
+# go through each flatpak program, ask if it should be installed and add it to the selected programs
+for program in "${flatpak_programs[@]}"; do
+      while true; do
+          read -p "Install $flatpak_program? [y/n] " yn
+          case $yn in
+              [Yy]* ) flatpak_selected_programs+=("$flatpak_program"); break;;
+              [Nn]* ) break;;
+              * ) echo "Enter 'y' or 'n'!";;
+          esac
+      done
+done
+
+# go through each appimage program, ask if it should be installed and add it to the selected programs
+for program in "${appimage_programs[@]}"; do
+      while true; do
+          read -p "Install $appimage_program? [y/n] " yn
+          case $yn in
+              [Yy]* ) appimage_selected_programs+=("$appimage_program"); break;;
+              [Nn]* ) break;;
+              * ) echo "Enter 'y' or 'n'!";;
+          esac
+      done
+done
+
+
+
+
+# Install the selected dnf programs
+dnf install -y "${dnf_selected_programs[@]}"
+
+# Install the selected free programs
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y "${free_selected_programs[@]}"
+
+# Install the selected nonfree programs
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y "${nonfree_selected_programs[@]}"
+
+# Install the selected flatpak programs
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update
+flatpak isntall "${flatpak_selected_programs[@]}"
+
+# Install AppImages (special for each one)
+for program in "${appimage_selected_programs[@]}"
+    if [[$program == "remnote"]]; then
+        curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote.sh > remnote.sh && chmod +x remnote.sh
+        ./remnote.sh
+        rm remnote.sh
+        break
+    fi
+done
+
