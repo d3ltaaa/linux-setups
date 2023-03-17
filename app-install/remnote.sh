9b038ce98e00631301439043cc886b24675ede39
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-18 00:12:52 +0100

    Update remnote.sh

--- app-install/remnote.sh
+++ app-install/remnote.sh
@@ -1,17 +1,12 @@
-
-echo "===> downloading remnote.AppImage"
-sleep 3
 mkdir -p /home/falk/.local/share/AppImages
 cd /home/falk/.local/share/AppImages
 wget --progress=bar:force -O remnote.AppImage https://www.remnote.com/desktop/linux
 chmod +x remnote.AppImage
 
-echo "===> Downloading remnote icon"
 mkdir -p /home/falk/.local/share/icons
 cd /home/falk/.local/share/icons
 wget https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote-install/remnote.png
 
-echo "===> Writing to remnote.desktop"
 mkdir -p /home/falk/.local/share/applications
 cat > /home/falk/.local/share/applications/remnote.desktop << EOF
 [Desktop Entry]
@@ -22,6 +17,4 @@ Icon=/home/falk/.local/share/icons/remnote.png
 Terminal=false
 EOF
 
-echo "===> making remnote.desktop executable"
-ls
 chmod +x remnote.desktop
