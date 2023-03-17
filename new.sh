9908246f77c6c8a365635bb527cd7bb0d587e82e
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-17 07:36:36 +0100

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,26 +8,26 @@ systemctl set-default graphical.target
 mkdir /home/falk/Downloads
 cd /home/falk/Downloads
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/dnf.sh
-dnf install dnf.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/dnf.sh > dnf.sh && chmod +x dnf.sh
+./dnf.sh
 rm dnf.sh
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/free.sh
-dnf install free.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/free.sh > free.sh && chmod +x free.sh
+./free.sh
 rm free.sh
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/non-free.sh
-dnf install non-free.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/non-free.sh > non-free.sh && chmod +x non-free.sh
+./non-free.sh
 rm non-free.sh
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote.sh
-dnf install remnote.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/remnote.sh > remnote.sh && chmod +x remnote.sh
+./remnote.sh
 rm remnote.sh
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/spotify.sh
-dnf install spotify.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/spotify.sh > spotify.sh && chmod +x spotify.sh
+./spotify.sh
 rm spotify.sh
 
-curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/synergy.sh
-dnf install synergy.sh
-rm synergy.sh
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/app-install/flatpak.sh > flatpak.sh && chmod +x flatpak.sh
+./flatpak.sh
+rm flapak.sh
