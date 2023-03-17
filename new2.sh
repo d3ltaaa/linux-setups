072c4b7a0e1cd53ef0c45b44f02936c002217a5b
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-17 23:32:38 +0100

    Update new2.sh

--- new2.sh
+++ new2.sh
@@ -2,9 +2,9 @@
 
 # Define the list of programs to install
 programs=(
-    git
-    neofetch
-    htop
+    program1
+    program2
+    program3
 )
 # Define the list of selected programs
 selected_programs=()
@@ -12,13 +12,14 @@ selected_programs=()
 # go through each program, ask if it should be installed and add it to the selected programs
 for program in "${programs[@]}"; do
       while true; do
-          read -p "Install $programs? [y/n] " yn
+          read -p "Install $program? [y/n] " yn
           case $yn in
-              [Yy]* ) selected_programs+=("$programs"); break;;
+              [Yy]* ) selected_programs+=("$program"); break;;
               [Nn]* ) break;;
-              * ) echo "Enter 'y' or 'n'!"; break;;
+              * ) echo "Enter 'y' or 'n'!";;
           esac
       done
 done
 
+# Install the selected programs
 sudo dnf install -y "${selected_programs[@]}"
