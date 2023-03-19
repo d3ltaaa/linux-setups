3cf451aac70259395e1b0cf3ab2bc65468eed1ab
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-19 14:15:54 +0100

    Update fix-xournalpp.sh

--- app-install/fix-xournalpp.sh
+++ app-install/fix-xournalpp.sh
@@ -3,4 +3,4 @@ echo "unset GDK_CORE_DEVICE_EVENTS" >> ~/.bashrc
 
 # Add "unset GDK_CORE_DEVICE_EVENTS;" before the "Exec=" line in the desktop file
 # and save the changes
-sudo sed -i 's|Exec=xournalpp %f|Exec=unset GDK_CORE_DEVICE_EVENTS;xournalpp %f|g' /usr/share/applications/xournalpp.desktop
+sudo sed -i 's|Exec=xournalpp %f|Exec=unset GDK_CORE_DEVICE_EVENTS;xournalpp %f|g' /usr/share/applications/com.github.xournalpp.xournalpp.desktop
