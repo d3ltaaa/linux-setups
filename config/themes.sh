62e05f3cf0ea79d835e7a66f02a241ba4f9113fc
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-22 16:15:50 +0100

    Create themes.sh

---
+++ config/themes.sh
@@ -0,0 +1,23 @@
+# colloid-icon-theme
+mkdir -p /home/falk/.themes
+cd /home/falk/.themes
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd /home/falk/.themes/Colloid-icon-theme
+./install.sh
+
+# colloid-global-kde-theme
+mkdir -p /home/falk/.themes
+cd /home/falk/.themes
+git clone https://github.com/vinceliuice/Colloid-kde.git
+cd /home/falk/.themes/Colloid-kde
+./install.sh
+
+# latte-dock
+mkdir -p /home/falk/.themes
+cd /home/falk/.themes
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/config/dock_updated_2.layout.latte > dock_updated_2.layout.latte 
+
+# ulauncher
+mkdir -p ~/.config/ulauncher/user-themes
+git clone https://github.com/Surendrajat/SeaOwl-Ulauncher-theme \
+~/.config/ulauncher/user-themes/SeaOwl
