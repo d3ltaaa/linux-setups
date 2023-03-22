3d194a48edb244ffa991143ec4894a832d8e2efb
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-03-22 16:10:20 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,6 @@
+# make certain programs execute on startup
+curl https://raw.githubusercontent.com/d3ltaaa/linux-setups/master/config/autostart.sh > autostart.sh && chmod +x autostart.sh
+./autostart.sh
+rm autostart.sh
 
+# install themes
