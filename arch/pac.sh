06c690fedabb5f9f7fea9fea30c19f3d51414e57
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-04-01 13:42:49 +0200

    Create pac.sh

---
+++ arch/pac.sh
@@ -0,0 +1,23 @@
+sudo su
+# base xorg
+pacman -S xf86-video-fbdev xorg xorg-xinit 
+# wm
+pacman -S awesome
+
+# packages - rice
+pacman -S nitrogen polybar rofi
+
+# packages - apps
+pacman -S firefox alacritty git xournalpp  flatpak
+
+# yay
+pacman -S base-devel
+mkdir /home/falk/.yay
+cd /home/falk/.yay
+git clone https://aur.archlinux.org/yay.git
+cd /home/falk/.yay/yay
+makepkg -si
+
+# packages - rice (yay)
+yay -S picom-jonaburg-fix
+yay -S font-manager-git
